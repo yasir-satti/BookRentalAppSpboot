7bed56a885afba2919b6443f0b3082b20ddeed84
Yasir Satti <yassir_satti@hotmail.com>
2021-11-04 10:38:04 +0000

    Create project repo

---
+++ src/main/java/com/project/bookrentalappspboot/BookRentalAppSpbootApplication.java
@@ -0,0 +1,13 @@
+package com.project.bookrentalappspboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRentalAppSpbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookRentalAppSpbootApplication.class, args);
+    }
+
+}
